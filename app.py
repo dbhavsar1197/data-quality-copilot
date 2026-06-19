a1e071a7ee1944928bfa401c7cdaeada11418301
Dhruv Bhavsar <bdhruv7@gmail.com>
2026-06-19 13:33:33 -0400

    Setup Streamlit project

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st #streamlite import 
+import datetime as datetime #toget current date and time. 
+
+user_timezone_str = datetime.context.timezone
+if user_timezone_str 
+ #--- Page congigration ----
+st.set_page_config(
+    page_title = "Data quality Copilot",
+    page_icon = "🤖")
+#--- Title ----
+st.title("D
